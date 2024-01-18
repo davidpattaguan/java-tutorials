3ea88be49741bdd2ceeb9292c00deb43c9e58f6c
David Niño Angelo Pattaguan <57347782+davidpattaguan@users.noreply.github.com>
2024-01-18 13:02:27 +0800

    Update ArraySamples.java

--- ArraySamples.java
+++ ArraySamples.java
@@ -1,2 +1,15 @@
 public class ArraySamples {
+
+
+    public static void ArraySample1() {
+
+        int value = 69;
+
+        int[] values = new int[3];
+
+        System.out.println(values[0] + " " + values[1] + " " + values[2]);
+
+    }
+
+
 }
